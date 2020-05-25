96505fb27d87434ede3167b3e9680cc70032695d
worud4417 <worud4417@gmail.com>
2020-05-26 00:14:53 +0900

    day2

--- javascript/fortress.js
+++ javascript/fortress.js
@@ -1,41 +1,57 @@
-const MOVELENGTH = 3
+const MOVELENGTH = 3.5
 const PLAYGROUNDX = "1000px"
-const PLAYGROUNDY = "500px"
+const PLAYGROUNDY = "450px"
 
+const TANKHEIGHT = "50px"
+const TANKWIDTH = "50px"
+
+const GUNBARRELHEIGHT = "3px"
+const GUNBARRELWIDTH = "70px"
 
 window.onload = function(){
     var div1 = this.document.getElementById("myDiv")
     var playground = this.document.getElementById("playground")
+    var gunBareel = this.document.getElementById("gunBarrel")
 
     playground.style.width = PLAYGROUNDX
     playground.style.height = PLAYGROUNDY
+
+    div1.style.height = TANKHEIGHT
+    div1.style.width = TANKWIDTH
+
+    gunBareel.style.height = GUNBARRELHEIGHT
+    gunBareel.style.paddingLeft = GUNBARRELWIDTH
+
+    div1.style.marginTop = (PLAYGROUNDY.slice(0,PLAYGROUNDY.length-2)*1)-(div1.style.height.slice(0,div1.style.height.length-2)*1)+"px"
 
     this.document.addEventListener("keydown",function(e){
         const keyCode = e.keyCode;
 
         if(keyCode == 39){
-            div1.style.marginLeft = xMoveTargetByMargin(div1.style.marginLeft,MOVELENGTH)
+            div1.style.marginLeft = moveTargetByMargin(div1.style.marginLeft,MOVELENGTH)
         }else if(keyCode == 37){
-            div1.style.marginLeft = xMoveTargetByMargin(div1.style.marginLeft,-MOVELENGTH)
+            div1.style.marginLeft = moveTargetByMargin(div1.style.marginLeft,-MOVELENGTH)
         }else if(keyCode == 38){
-            div1.style.marginTop = yMoveTargetByMargin(div1.style.marginTop,-MOVELENGTH)
+
         }else if(keyCode == 40){
-            div1.style.marginTop = yMoveTargetByMargin(div1.style.marginTop,MOVELENGTH)
+
         }
     })
 }
 
 
-function xMoveTargetByMargin(_originValue, _moveLength){
+function moveTargetByMargin(_originValue, _moveLength){
     var number = _originValue.slice(0,_originValue.length-2)*1
     var movepoint = number+_moveLength
-    var stringMovePoint = movepoint+"px"
-    return stringMovePoint
-}
 
-function yMoveTargetByMargin(_originValue, _moveLength){
-    var number = _originValue.slice(0,_originValue.length-2)*1
-    var movepoint = number+_moveLength
+    var maxPoint = (PLAYGROUNDX.slice(0,PLAYGROUNDX.length-2)*1)-(TANKWIDTH.slice(0,TANKWIDTH.length-2)*1)
+
+    if(movepoint<0){
+        movepoint = 0
+    } else if(movepoint>maxPoint){
+        movepoint = maxPoint
+    }
+
     var stringMovePoint = movepoint+"px"
     return stringMovePoint
 }
